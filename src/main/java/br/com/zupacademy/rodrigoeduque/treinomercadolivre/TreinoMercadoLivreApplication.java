5966bb0af913ed307ade09a2af7e5964434ab751
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-07-07 09:25:20 -0300

    setup do projeto mercado-livre

---
+++ src/main/java/br/com/zupacademy/rodrigoeduque/treinomercadolivre/TreinoMercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rodrigoeduque.treinomercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoMercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoMercadoLivreApplication.class, args);
+	}
+
+}
